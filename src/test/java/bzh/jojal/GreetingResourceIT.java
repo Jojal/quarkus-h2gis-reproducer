fc6784d004d0c21b4182f4a9a9569a286a78ce66
Jordan <jojal@jojal.bzh>
2023-11-02 09:55:13 +0100

    feat: postgis OK

---
+++ src/test/java/bzh/jojal/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package bzh.jojal;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
