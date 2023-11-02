9e871fd6ecacb645dd4abaedc15bec99fef6cb5e
Jordan <jojal@jojal.bzh>
2023-11-02 14:07:32 +0100

    No need to enable H2GIS with SQL COMMAND at startup. Hibernate will do it

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-CREATE ALIAS IF NOT EXISTS H2GIS_SPATIAL FOR "org.h2gis.functions.factory.H2GISFunctions.load";
-CALL H2GIS_SPATIAL();
+-- CREATE ALIAS IF NOT EXISTS H2GIS_SPATIAL FOR "org.h2gis.functions.factory.H2GISFunctions.load";
+-- CALL H2GIS_SPATIAL();
 
-INSERT INTO location(id,name,geom) VALUES (0,'test', ST_PointFromText('POINT(550700 5801000)', 25832));
+INSERT INTO location(id,name,geom) VALUES (0,'test', ST_PointFromText('POINT(530700 5801000)', 25832));
