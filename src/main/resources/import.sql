277b183fc7e83a574bd59c182361f5b1ed577bf2
Jordan <jojal@jojal.bzh>
2023-11-04 11:58:05 +0100

    Ok for native + non native + quarkus:dev

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
--- CREATE ALIAS IF NOT EXISTS H2GIS_SPATIAL FOR "org.h2gis.functions.factory.H2GISFunctions.load";
--- CALL H2GIS_SPATIAL();
+CREATE ALIAS IF NOT EXISTS H2GIS_SPATIAL FOR "org.h2gis.functions.factory.H2GISFunctions.load";
+CALL H2GIS_SPATIAL();
 
 INSERT INTO location(id,name,geom) VALUES (0,'test', ST_PointFromText('POINT(-4 48)', 4326));
 INSERT INTO location(id,name,geom) VALUES (1,'polygon', ST_PolyFromText('POLYGON((10 -25, 34 -20, 38 -35, 13 -39, 10 -25))', 4326));
